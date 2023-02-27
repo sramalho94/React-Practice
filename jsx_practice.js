302b9373023d78e8161930f50f89f1794ce99683
Stephan D Ramalho <sramalho@fordham.edu>
2023-02-27 13:50:06 -0500

    practice with jsx

---
+++ jsx_practice.js
@@ -0,0 +1,12 @@
+// 1.Declare a constant named p1.
+// Set p1 equal to a JSX <p></p> element. Write the word foo in between the <p></p> tags.
+const p1 = <p>foo</p>
+
+// 2.On the next line, declare a constant named p2.
+// Set p2 equal to another JSX <p></p> element. Write the word bar in between the <p></p> tags.
+const p2 = <p>bar</p>
+
+// 3.Give the first <p></p> an id attribute of 'large'.
+// Give the second <p></p> an id attribute of 'small'.
+// const p1 = <p id='large'>foo</p>
+// const p2 = <p id='small'>bar</p>
