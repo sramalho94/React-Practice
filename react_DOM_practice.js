d407eb52fe7a78c48be03848ab07caeba385d2da
Stephan D Ramalho <sramalho@fordham.edu>
2023-02-27 14:05:49 -0500

    more practice with rendering jsx elements

--- react_DOM_practice.js
+++ react_DOM_practice.js
@@ -12,3 +12,26 @@ import ReactDOM from 'react-dom'
 
 // Write code here:
 ReactDOM.render(<h1>Render me!</h1>, document.getElementById('app'))
+
+// 1.
+// On line 5, declare a variable named myList. Set myList equal to a JSX <ul></ul> element. Wrap your <ul></ul> in parentheses.
+
+// Add several <li></li> elements in between your <ul></ul> tags. Put some text in each <li></li>. Use line breaks and indentation similar to the above example.
+
+// 2.
+// At the bottom of the file, call ReactDOM.render().
+
+// For ReactDOM.render()‘s first argument, pass in the variable myList.
+
+// For ReactDOM.render()‘s second argument, select an HTML element with an id of app.
+
+// Feel free to use the example code as a guide.
+// Write code here:
+const myList = (
+  <ul>
+    <li>item 1</li>
+    <li>item 2! </li>
+  </ul>
+)
+
+ReactDOM.render(myList, document.getElementById('app'))
