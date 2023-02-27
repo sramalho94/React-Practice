e6105e4678bf1ce3c78332bd22648a32c04d4042
Stephan D Ramalho <sramalho@fordham.edu>
2023-02-27 13:59:57 -0500

    practice with rendering jsx elements

---
+++ react_DOM_practice.js
@@ -0,0 +1,14 @@
+// Below the import statements, call ReactDOM.render().
+
+// Pass in this expression as a first argument:
+
+// <h1>Render me!</h1>
+// Pass in this expression as a second argument:
+
+// document.getElementById('app')
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// Write code here:
+ReactDOM.render(<h1>Render me!</h1>, document.getElementById('app'))
