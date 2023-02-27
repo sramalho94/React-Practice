6551aa32734f980af78fb5c1486ff422d3826113
Stephan D Ramalho <sramalho@fordham.edu>
2023-02-27 13:53:04 -0500

    practice with nested jsx elements

---
+++ nested_jsx_practice.js
@@ -0,0 +1,7 @@
+// 1.Declare a new variable named myDiv. Set myDiv equal to a JSX <div></div> element.
+// Wrap the <div></div> in parentheses, and use indentation and line breaks like in the examples. In between the <div></div> tags, nest an <h1></h1> containing the text Hello world.
+const myDiv = (
+  <div>
+    <h1>Hello world</h1>
+  </div>
+)
