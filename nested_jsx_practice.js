ebe85626322b6fc46b73905118f5fb7b8239c0f2
Stephan D Ramalho <sramalho@fordham.edu>
2023-02-27 13:54:39 -0500

    more practice with nesting jsx elements

--- nested_jsx_practice.js
+++ nested_jsx_practice.js
@@ -5,3 +5,20 @@ const myDiv = (
     <h1>Hello world</h1>
   </div>
 )
+
+// 1.Your friend’s blog is down! He’s asked you to fix it.
+
+// You immediately diagnose the problem: a JSX expression with multiple outer elements.
+
+// Repair your friend’s broken code by wrapping their JSX in a <div></div>.
+
+const blog = (
+  <div>
+    <img src="pics/192940u73.jpg" />
+    <h1>Welcome to Dan's Blog!</h1>
+    <article>
+      Wow I had the tastiest sandwich today. I <strong>literally</strong> almost
+      freaked out.
+    </article>
+  </div>
+)
