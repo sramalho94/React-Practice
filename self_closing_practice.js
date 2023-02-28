203fe375c2393fa6ae123f11744e426a913f02dd
Stephan D Ramalho <sramalho@fordham.edu>
2023-02-28 12:45:20 -0500

    practice with self closing jsx elements

---
+++ self_closing_practice.js
@@ -0,0 +1,14 @@
+// In app.js, fix the broken JSX by adding slashes to all of the self-closing tags.
+const profile = (
+  <div>
+    <h1>I AM JENKINS</h1>
+    <img src="images/jenkins.png" />
+    <article>
+      I LIKE TO SIT
+      <br />
+      JENKINS IS MY NAME
+      <br />
+      THANKS HA LOT
+    </article>
+  </div>
+)
